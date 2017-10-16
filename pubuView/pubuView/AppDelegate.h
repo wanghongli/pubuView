5d987056795a2d5353eda68f643cc167070deb62
王红力 <wanghongli@zhaojiafang.com>
2017-10-16 22:18:29 +0800

    [sunrise 创建工程]

---
+++ pubuView/pubuView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pubuView
+//
+//  Created by sunrise on 2017/10/16.
+//  Copyright © 2017年 sunrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pubuView/pubuView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pubuView
+//
+//  Created by sunrise on 2017/10/16.
+//  Copyright © 2017年 sunrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
